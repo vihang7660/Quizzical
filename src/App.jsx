f27284395bb3ff7a485f10daf827a96b73ca35a9
Vihang Supare <vihang7660@gmail.com>
2022-10-19 18:19:29 +0530

    Modified state management as well as added ability to select answers

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,34 @@ import { nanoid } from "nanoid";
 function App() {
   const [information, setInformation] = React.useState([]);
 
+  const [count, setCount] = React.useState(0);
+
   React.useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=10")
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy")
       .then((resp) => resp.json())
-      .then((data) => setInformation(data.results));
+      .then((data) => {
+        let details = data.results.map((item) => {
+          item.id = nanoid();
+          item.options = item.incorrect_answers.map((data) => {
+            let dataInfo = {
+              isHeld: false,
+              value: data,
+              id: nanoid(),
+            };
+            return dataInfo;
+          });
+          item.options.push({
+            isHeld: false,
+            value: item.correct_answer,
+            id: nanoid(),
+          });
+          shuffle(item.options);
+          return item;
+        });
+        setInformation(details);
+      });
   }, []);
+
   function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
@@ -17,26 +40,24 @@ function App() {
     }
   }
 
-  let info = information.map((item) => {
-    /* let options = [...item.incorrect_answers, item.correct_answer]; */
-    let options = item.incorrect_answers.map((data) => {
-      return { isHeld: false, value: data };
-    });
-    options.push({ isHeld: false, value: item.correct_answer });
-    shuffle(options);
-    
+  let info = information.map((item, index) => {
     return (
       <Question
         ques={item.question}
-        correct={item.correct_answer}
-        incorrect={item.incorrect_answers}
-        options={options}
+        options={item.options}
+        setInformation={setInformation}
+        id={item.id}
         key={nanoid()}
       />
     );
   });
 
-  return <div>{info}</div>;
+  return (
+    <div>
+      {info}
+      <h2 onClick={() => setCount(count + 1)}>{count}</h2>
+    </div>
+  );
 }
 
 export default App;
--- src/Question.jsx
+++ src/Question.jsx
@@ -3,11 +3,32 @@ import { decode } from "html-entities";
 import { nanoid } from "nanoid";
 
 export default function Question(props) {
+  function toggle(id, Qid) {
+    props.setInformation((prevState) =>
+      prevState.map((item) => {
+        return item.id === Qid
+          ? {
+              ...item,
+              options: item.options.map((data) =>
+                data.id === id
+                  ? { ...data, isHeld: !data.isHeld }
+                  : { ...data, isHeld: false }
+              ),
+            }
+          : item;
+      })
+    );
+  }
+
   let option = props.options.map((item) => {
-    let style = { backgroundColor: item.isHeld ? "green" : "gray" };
+    let style = { backgroundColor: item.isHeld ? "green" : "aqua" };
 
     return (
-      <h5 style={style} key={nanoid()}>
+      <h5
+        style={style}
+        key={nanoid()}
+        onClick={() => toggle(item.id, props.id)}
+      >
         {decode(item.value)}
       </h5>
     );
